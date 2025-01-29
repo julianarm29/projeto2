6c5c539fb6f961d97f4b3759244c5bd8d436c029
Juliana Rodrigues <juliana.rodrigues-29@hotmail.com>
2025-01-29 22:34:14 +0100

    add build

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Atualiza o pip para a versão mais recente
+python3 -m pip install --upgrade pip
+
+# Instala as dependências do requirements.txt
+python3 -m pip install -r requirements.txt
