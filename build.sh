6b5f1ea5b8bce99fe61bfef1bfd42db59aed886c
Juliana Rodrigues <juliana.rodrigues-29@hotmail.com>
2025-01-31 16:13:12 +0100

    deleting files

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Atualiza o pip para a versão mais recente
-python3 -m pip install --upgrade pip
-
-# Instala as dependências do requirements.txt
-python3 -m pip install -r requirements.txt
