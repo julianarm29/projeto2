3bd8a73d572d179de3193bffbe0fa608a6850db6
Juliana Rodrigues <juliana.rodrigues-29@hotmail.com>
2025-01-31 17:50:00 +0100

    add host

--- social/settings.py
+++ social/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-0d&@&#$+iplcor#dzu50(oe3d#e$bakyoe@5!ydmteqk%&%w9l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['julianarm29.pythonanywhere.com']
+ALLOWED_HOSTS = ['julianarm29.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
